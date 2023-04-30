2c49e67d505bef23d7f049feaed13bced09ea05f
dhruboJaman <tarifjaman007@gmail.com>
2023-04-30 18:12:35 +0600

    bat

---
+++ bat.js
@@ -0,0 +1,14 @@
+
+let batman = document.getElementById("count-el")
+
+let count = 0
+
+
+
+
+function batfnc() {
+    count += 1
+    console.log("clicked")
+    batman.textContent = count 
+    
+}
